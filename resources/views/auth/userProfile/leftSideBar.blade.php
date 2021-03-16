79e3942af0505678aae0339d2f410f31a15dfeef
BranLuna <BranLuna.Work@Gmail.com>
2021-03-15 21:30:44 -0600

    Added&Styled the profile tab panes & side bar

--- resources/views/auth/userProfile/leftSideBar.blade.php
+++ resources/views/auth/userProfile/leftSideBar.blade.php
@@ -1,21 +1,32 @@
-<div class="col-md-2 leftbar-cont2 text-center">
-  <a
+<div class="col-md-2 d-flex flex-column text-center leftbar-cont2">
+  <div>
+    <img src="assets/images/UserDefault.png" alt="User Pic here">
+  </div>
+  <div class="mt-3">
+    <h4>{{$user->name}}</h4>
+  </div>
+  
+  <nav class="nav flex-column nav-pills my-4 text-left h-100" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+    <a class="nav-link leftbar-nav-link active" id="v-pills-cotizaciones-tab" data-toggle="pill" href="#v-pills-cotizaciones" role="tab" aria-controls="v-pills-cotizaciones" aria-selected="true">Cotizaciones</a>
+    <a class="nav-link leftbar-nav-link" id="v-pills-reservas-tab" data-toggle="pill" href="#v-pills-reservas" role="tab" aria-controls="v-pills-reservas" aria-selected="false">Reservas</a>
+    <a class="nav-link leftbar-nav-link" id="v-pills-preAprobaciones-tab" data-toggle="pill" href="#v-pills-preAprobaciones" role="tab" aria-controls="v-pills-preAprobaciones" aria-selected="false">Pre Aprobaciones</a>
+    <a class="nav-link leftbar-nav-link" id="v-pills-propiedadesFavoritas-tab" data-toggle="pill" href="#v-pills-propiedadesFavoritas" role="tab" aria-controls="v-pills-propiedadesFavoritas" aria-selected="false">Propiedades Favoritas</a>
+    <a class="nav-link leftbar-nav-link mt-auto" id="v-pills-perfil-tab" data-toggle="pill" href="#v-pills-perfil" role="tab" aria-controls="v-pills-perfil" aria-selected="false"><i class="fas fa-cog main-color"></i> Configuración</a>
+  </nav>
+
+  <div class="mb-2">
+    <a
     href="javascript:void(0);"
-    class="main-color logoutLink"
+    class="btn bg-main-color navBar-btn text-light logoutLink"
     onclick="event.preventDefault(); document.getElementById('logout').submit();">
     Terminar Sesion
     <i class="fas fa-sign-out-alt"></i>
-  </a>
-  <form id="logout"
-    action="{{ route('logout')}}"
-    method="POST"
-    style="display: none;"
-    >@method('PUT') @csrf
-  </form>
-  <div class="nav flex-column nav-pills mt-2" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-    <a class="nav-link main-color active" id="v-pills-perfil-tab" data-toggle="pill" href="#v-pills-perfil" role="tab" aria-controls="v-pills-perfil" aria-selected="true">Perfil</a>
-    <a class="nav-link main-color" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-    <a class="nav-link main-color" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-    <a class="nav-link main-color" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+    </a>
+    <form id="logout"
+      action="{{ route('logout')}}"
+      method="POST"
+      style="display: none;"
+      >@method('PUT') @csrf
+    </form>
   </div>
 </div>
---
+++ resources/views/components/profile/profileTabPanes/reservas.blade.php
@@ -0,0 +1,33 @@
+<div class="card shadow-sm">
+  <div class="card-body">
+    <h3 class="card-title">Reservas</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Fecha creación</th>
+          <th>Proyecto</th>
+          <th>Unidad Principal</th>
+          <th>Unidad Adicionales</th>
+          <th>Vigencia reserva</th>
+          <th><i class="far fa-file-pdf"></i></th>
+          <th><i class="far fa-file-archive"></i></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>-</td>
+          <td>-</td>
+          <td>-</td>
+          <td>-</td>
+          <td>-</td>
+          <td><a class="main-color" href=""><i class="far fa-file-pdf"></i></a></td>
+          <td><a class="main-color" href=""><i class="far fa-file-archive"></i></a></td>
+        </tr>
+      </tbody>
+    </table>
+    <hr>
+    <div class="text-right">
+      pagination here
+    </div>
+  </div>
+</div>
